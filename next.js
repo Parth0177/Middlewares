c6dd5a8ae345c9d09295c0081157b1a53bf8cdaa
Parth Tiwari <parthtiwari670@gmail.com>
2025-06-30 00:48:32 +0530

    feat: add initial Express middleware and routes

---
+++ next.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.use((req,res,next)=>{
+  console.log('This is a Middleware function!');
+  next(); // Call next() to pass control to the next middleware or route handler
+})
+
+app.get('/',(req,res)=>{
+  res.send('Hello World!!');
+});
+
+app.get('/random', (req,res)=>{
+  const randomNumber = Math.floor(Math.random() * 100);
+  res.send(`Random Number is: ${randomNumber}`);
+})
+
+app.listen(3000,()=>{
+  console.log('Server is running on port 3000');
+  
+})
