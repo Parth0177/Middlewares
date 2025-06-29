e15d4a0b16d8cc6ac41cb08216b4487df47ba051
Parth Tiwari <parthtiwari670@gmail.com>
2025-06-29 14:19:52 +0530

    initialize express application with middleware and routes

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.use((req,res)=>{
+  console.log('This is a middleware function');
+  res.send('Middleware executed');
+})
+
+app.get('/',(req,res)=>{
+  res.send('Hello World!!');
+});
+
+app.get('/random', (req,res)=>{
+  const randomNumber = Math.floor(Math.random() * 100);
+  res.send(`Random number is: ${randomNumber}`);
+})
+
+app.listen(3000,()=>{
+  console.log('Server is running on 3000');
+  
+})
