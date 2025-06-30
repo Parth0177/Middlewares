da502407775ace4ecabe9c978d0e330d8594f6df
Parth Tiwari <parthtiwari670@gmail.com>
2025-06-30 13:11:36 +0530

    fix: improve error handling middleware and ensure proper response formatting in Error2.js

--- Error2.js
+++ Error2.js
@@ -12,12 +12,16 @@ app.use("/api", (req,res,next)=>{
   throw new ExpressError("You don't have access to this API", 401);
 });
 
+app.use((err,req,res,next)=>{
+  let {statusCode=500,message} = err;
+  res.status(statusCode).send(message)
+});
 
 app.get('/api', (req,res)=>{
-  res.send('data')
-})
+  res.send('data');
+});
 
 
 app.listen(3001, ()=>{
   console.log('Server is running on port 3001');
-})
+});
