528ed26c8895c9c1d8d1cc48fd89ed581856861d
Parth Tiwari <parthtiwari670@gmail.com>
2025-06-30 11:56:03 +0530

    fix: correct error handling middleware implementation and syntax in error.js

--- error.js
+++ error.js
@@ -10,10 +10,21 @@ app.use("/api", (req,res,next)=>{
     res.status(401).send('Unauthorized access');
   }
 });
-
 app.get('/wrong',(req,res)=>{
   abcd = abcd ;
-})
+});
+
+app.use((err,req,res,next)=>{
+  console.log("ERROR OCCURED!");
+  next(err);
+});
+
+app.use((err,req,res,next)=>{
+  console.log("ERROR HANDLER!");
+  res.status(500).send('Something went wrong!');
+  next(err);
+});
+
 
 app.get('/api', (req,res)=>{
   res.send('data')
